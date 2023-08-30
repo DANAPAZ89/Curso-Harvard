d791f0859a304cb62fdd6ef1b5237ed6f44cf8a9
Diego <danapaz1989@gmail.com>
2023-08-30 21:30:34 +0300

    Añadido ejercicio Leet

--- Python/src/ejercicios_python/Challenges.py
+++ Python/src/ejercicios_python/Challenges.py
@@ -153,3 +153,35 @@ def invert_str(string):
 
 word = input("Escribe el texto/palabra que desee invertir: ")
 print(invert_str(word))
+
+"""
+"EL LENGUAJE HACKER"
+Escribe un programa que reciba un texto y transforme lenguaje natural a
+ "lenguaje hacker" (conocido realmente como "leet" o "1337"). Este lenguaje
+ se caracteriza por sustituir caracteres alfanuméricos.
+ - Utiliza esta tabla (https://www.gamehouse.com/blog/leet-speak-cheat-sheet/) 
+   con el alfabeto y los números en "leet".
+   (Usa la primera opción de cada transformación. Por ejemplo "4" para la "a")
+
+"""
+
+import random 
+
+def hacker_language(text):
+        
+    leet = {"A": "4", "B": "I3", "C": "[", "D": ")", "E": "3", "F": "|=", "G": "&", "H": "#", "I": "1",
+        "J": ",_|", "K": ">|", "L": "1", "M": "/\/\ ", "N": "^/", "O": "0", "P": "|*", "Q": "(_,)",
+        "R": "I2", "S": "5", "T": "7", "U": "(_)", "V": "\/", "W": "\/\/", "X": "><", "Y": "j", "Z": "2",
+        "1": "L", "2": "R", "3": "E", "4": "A", "5": "S", "6": "b", "7": "T", "8": "B", "9": "g", "0": "o"}
+    
+    leet_text = ""
+    
+    for letter in text:
+        if letter.upper() in leet.keys():
+            leet_text += leet[letter.upper()]
+        else:
+            leet_text += letter
+    
+    return leet_text
+
+print(hacker_language(input("Introduzca el texto a cifrar: ")))
